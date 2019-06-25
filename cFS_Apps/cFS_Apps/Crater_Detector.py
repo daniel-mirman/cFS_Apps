efb7d596112f32bda42cb70ab48841647e243905
daniel-mirman <51454916+daniel-mirman@users.noreply.github.com>
2019-06-25 15:54:53 -0400

    Add files via upload

--- cFS_Apps/cFS_Apps/Crater_Detector.py
+++ cFS_Apps/cFS_Apps/Crater_Detector.py
@@ -9,25 +9,25 @@ import glob
 import numpy as np
 
 
-image= glob.glob("/Users/danielmirman/Desktop/messier4K.tiff")[0]
+image= glob.glob("/Users/danielmirman/Desktop/Moon_Photo1.jpg")[0]
 grayscale = imread(image, as_gray=True)
 
 # Compute radii in the 3rd column.
 blobs_log = blob_log(grayscale, max_sigma=30, num_sigma=10, threshold=.1)
 blobs_log[:, 2] = blobs_log[:, 2] * sqrt(2)
-numStars1 = len(blobs_log)
-stars1 = print('Number of stars counted Photo 1: ', numStars1)
+numStars1= len(blobs_log)
+print('Number of stars counted Photo 1: ', numStars1)
 
 
 blobs_dog = blob_dog(grayscale, max_sigma=30, threshold=.1)
 blobs_dog[:, 2] = blobs_dog[:, 2] * sqrt(2)
-numStars2 = len(blobs_dog)
-stars2 = print('Number of stars counted Photo 2: ', numStars2)
+numStars2= len(blobs_dog)
+print('Number of stars counted Photo 2: ', numStars2)
 
 
-blobs_doh = blob_doh(grayscale, max_sigma=30, threshold=.01)
-numStars3 = len(blobs_doh)
-stars3 = print('Number of stars counted Photo 3: ', numStars3)
+blobs_doh = blob_doh(grayscale, max_sigma=30, threshold=.1)
+numStars3= len(blobs_doh)
+print('Number of stars counted Photo 3: ', numStars3)
 
 
 
@@ -36,10 +36,8 @@ blobs_list = [blobs_log, blobs_dog, blobs_doh]
 # Visualization Information
 colors = ['lime', 'yellow', 'red']
 
-#stars = [stars1, stars2, stars3]
-
-titles = ['Laplacian of Gaussian', 'Difference of Gaussian', 
-	  'Difference of Hessian']
+titles = ['Laplacian of Gaussian', 'Difference of Gaussian',
+          'Determinant of Hessian']
 
 sequence = zip(blobs_list, colors, titles)
 
@@ -48,7 +46,6 @@ ax = axes.ravel()
 
 for idx, (blobs, color, title) in enumerate(sequence):
     ax[idx].set_title(title)
-    #ax[idx].set_title(star)
     ax[idx].imshow(grayscale, cmap='gray', interpolation='nearest')
     
     for blob in blobs:
